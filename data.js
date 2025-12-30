5d9a8f8e515cd6f3a2b80f08c112a0740729fbe8
LIWENDA888 <744223766@qq.com>
2025-12-30 10:24:09 +0800

    Add files via upload

--- data.js
+++ data.js
@@ -16,6 +16,7 @@ const SEARCH_ENGINES = [
 const HEADER_LINKS = [
     { title: '自在造字首页', url: 'https://www.zizao.top' },
     { title: '字体产品', url: 'https://www.zizao.top/fonts' },
+    { title: '可变字体空间', url: 'https://vf.zizao.top' },
     { title: '设计百宝库', url: 'https://bao.zizao.top' },
     { title: '超引形近字', url: 'https://chaoyin.zizao.top' },
     { title: '文本编辑器Pro', url: 'https://tools.zizao.top/txt' },
